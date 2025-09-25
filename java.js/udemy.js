5588d21cc0210a9cf53db184a1bca19836739e95
minajson <minajjumbo@gmail.com>
2025-09-25 05:13:40 +0100

    Added solutoon forr challenege to solve Mark and John BMI

--- java.js/udemy.js
+++ java.js/udemy.js
@@ -1,5 +1,5 @@
 
-const country = "Nigeria";
+/*const country = "Nigeria";
 const continent = "Africa"
 let population = 1000000000;
 console.log(country);
@@ -13,7 +13,7 @@ console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
 
-language = "English";
+language = "English";*/
 
 console.log(population/2);
 population++;
@@ -23,3 +23,18 @@ console.log(population<33);
 const description1 = country + "is in" + continent +  "and it's" + population + "million peopple speak" + languauge;
 console.log(description1)
 
+
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn  = 92;
+let heightJohn = 1.95;
+let BMIMark= massMark/ (heightMark * heightMark)
+let BMIJohn= massJohn/ (heightJohn * heightJohn)
+
+
+console.log(BMIMark);
+console.log(BMIJohn);
+
+
+let markHigherBMI = (heightMark > BMIJohn) ;
+console.log(markHigherBMI);
