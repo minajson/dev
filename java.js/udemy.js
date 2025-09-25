c242e63523644e3fb24c5c98243592a4d247225c
minajson <minajjumbo@gmail.com>
2025-09-25 05:03:13 +0100

    Added jonas udemy assignment

---
+++ java.js/udemy.js
@@ -0,0 +1,25 @@
+
+const country = "Nigeria";
+const continent = "Africa"
+let population = 1000000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+let language = "Ibani"
+console.log(isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = "English";
+
+console.log(population/2);
+population++;
+console.log(population);
+console.log(population>6);
+console.log(population<33);
+const description1 = country + "is in" + continent +  "and it's" + population + "million peopple speak" + languauge;
+console.log(description1)
+
